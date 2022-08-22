b2e2266a735845ffb2a4d5bdcad6dc7afc03f98f
unknown <kh0shmaze@gmail.com>
2022-08-22 21:48:29 +0430

    outputing data page & notfound page & prompt

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning react is fun!" },
+  { id: "q2", author: "Maximilian", text: "Learning react for great!" },
+];
+
 const AllQuotes = () => {
-  return <h1> The AllQuotes page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div className="centered">
+      <h1>404 Error</h1>
+
+      <p>Page Not Found!</p>
+    </div>
+  );
+};
+
+export default NotFound;
